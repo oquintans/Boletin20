1fd3b8ef8ddfdc0ed714d276d65cbb2f24cd41be
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2015-02-19 10:49:52 +0100

    metodo remove terminado

--- src/boletin20/Colegio.java
+++ src/boletin20/Colegio.java
@@ -31,6 +31,7 @@ public class Colegio {
 
             String n = JOptionPane.showInputDialog("Introducir Nombre");
             int nt = Integer.parseInt(JOptionPane.showInputDialog("Introducir Nota"));
+
             String r = String.valueOf(reg);
             alum.put(r, new Alumno(n, nt));
             reg++;
@@ -42,7 +43,15 @@ public class Colegio {
         int conf;
         do {
             String r = JOptionPane.showInputDialog("Introducir Registro");
-            alum.remove(r);
+            boolean x = alum.containsKey(r);
+            if (x) {
+                int p = JOptionPane.showConfirmDialog(null, "Seguro que quiere borrar al alumno " + r);
+                if (p == 0) {
+                    JOptionPane.showMessageDialog(null, alum.remove(r) + " ha sido borrado");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el alumno con registro: " + r);
+            }
             conf = JOptionPane.showConfirmDialog(null, "Dar de baja otro alumno?");
         } while (conf == 0);
     }
@@ -51,11 +60,11 @@ public class Colegio {
         int conf;
         do {
             String r = JOptionPane.showInputDialog("Introducir Registro");
-            boolean x=alum.containsKey(r);
-            if(x){
-                JOptionPane.showMessageDialog(null,alum.get(r));
-            }else{
-                JOptionPane.showMessageDialog(null,"No existe el alumno con registro: " + r);
+            boolean x = alum.containsKey(r);
+            if (x) {
+                JOptionPane.showMessageDialog(null, alum.get(r));
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe el alumno con registro: " + r);
             }
             conf = JOptionPane.showConfirmDialog(null, "Consultar otro alumno?");
         } while (conf == 0);
